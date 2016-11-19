471eb470f8a4a849bb40005438d6d29f1cd7e095
Tobias Fuchs <twh.fuchs@gmail.com>
2016-11-19 16:03:01 +0100

    Added code and solution for assignment-03

--- assignment-03/s-03/solution/sparse_array.h
+++ assignment-03/s-03/solution/sparse_array.h
@@ -1,7 +1,6 @@
 #ifndef CPPPC__S03__SPARSE_ARRAY_H__INCLUDED
 #define CPPPC__S03__SPARSE_ARRAY_H__INCLUDED
 
-// Might come in handy:
 #include <unordered_map>
 
 
@@ -12,23 +11,32 @@ namespace detail {
 template <class SparseArrayT>
 class sparse_array_iterator
 {
-  // Just a suggestion
+public:
+
+  sparse_array_iterator(
+    SparseArrayT & sparse_array,
+    int            pos = 0)
+  : _sparse_array(sparse_array),
+    _pos(pos)
+  { }
+
+private:
+
+  SparseArrayT & _sparse_array;
+  int            _pos = 0;
+
 };
 
 template <class SparseArrayT>
 class sparse_array_proxy_ref
 {
-  // Just a suggestion
 };
 
 } // namespace detail
 
-
 template <class T, std::size_t N>
 class sparse_array
 {
-  // Just a suggestion:
-
   typedef sparse_array<T, N>                      self_t;
   typedef detail::sparse_array_proxy_ref<self_t>  proxy_reference;
   typedef int                                     index_t;
@@ -42,7 +50,30 @@ public:
 
 public:
 
-  // YOUR IMPLEMENTATION HERE
+  sparse_array(T default_value = T())
+  : _default(default_value),
+    _begin(*this, 0),
+    _end(*this, N)
+  { }
+
+  iterator begin() const {
+    return _begin;
+  }
+
+  iterator end() const {
+    return _end;
+  }
+
+  proxy_reference operator[](int index) {
+    return proxy_reference(*this, index);
+  }
+
+private:
+
+  T                              _default = 0;
+  std::unordered_map<index_t, T> _values;
+  iterator                       _begin;
+  iterator                       _end;
 
 };
 
