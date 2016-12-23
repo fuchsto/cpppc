ff644e6d8eece2070b211d4fd9fc267769913d87
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-12-23 01:01:08 +0100

    Session 06 examples

--- session-07/02-meta/01-constexpr/main.cc
+++ session-07/02-meta/01-constexpr/main.cc
@@ -1,4 +1,18 @@
 
+// See: http://en.cppreference.com/w/cpp/concept/LiteralType
+class Point {
+  int _x;
+  int _y;
+public:
+  constexpr Point(int ix, int iy) : _x{ix}, _y{iy} { }
+  constexpr int x() const { return _x; }
+  constexpr int y() const { return _y; }
+};
+
+
+// Q: This works for char, int, long, ... but: why can't we use float
+//    or double?
+//
 template <long N>
 struct fibonacci_tpl {
   static const long value = fibonacci_tpl<N-1>::value +
@@ -15,7 +29,11 @@ struct fibonacci_tpl<2> {
   static const long value = 1;
 }
 
-
+// Q: Could we use float or double here?
+//
+//    What if the code is compiled on a system with an FPU type
+//    different from the FPU at the system the program is run?
+//
 constexpr long fibonacci(long i) {
   return (i <= 1u) ? i : (fibonacci(i-1) + fibonacci(i-2));
 }
@@ -30,6 +48,13 @@ int main(int argc, char * argv[])
   std::cout << sizeof(int_values) << std::endl;
   std::cout << fib_9              << std::endl;
   std::cout << fibonacci(argc)    << std::endl;
+
+  constexpr Point p { 23, 42 };
+  constexpr int py = p.y();
+  double darr[py] {};
+
+  // Q: This is identical to instance p. Can the compiler memoize it?
+  constexpr Point p2 { 23, 42 };
 
 #if 0
   // Q: Why does this fail?
