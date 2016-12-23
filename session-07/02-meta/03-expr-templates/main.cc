9e3dac14f6c96da320ab028d679fa31ead57da27
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-12-23 03:24:00 +0100

    Session 06 examples

--- session-07/02-meta/03-expr-templates/main.cc
+++ session-07/02-meta/03-expr-templates/main.cc
@@ -6,18 +6,40 @@
 
 int main()
 {
+  // Now everybody, sing along!
+  
+  
+  //
+  // "With your state on the stack and your op well-defined ..."
+  //
   cpppc::vector<int> v1 { 1, 2, 3, 4 };
   cpppc::vector<int> v2 { 5, 6, 7, 8 };
   cpppc::vector<int> v3 { 9, 0, 1, 2 };
 
+  // 
+  // "... try this trick, and spin it, yeah ..."
+  //
   std::cout << "instantiate vector_sum:" << std::endl;
   auto vsum = v1 + v2;
+
+  //
+  // "Your op will collapse, 'cause there's nothing in it, and
+  //  you ask yourself: where is my sum?"
+  //
 
   std::cout << "assign vector_sum:" << std::endl;
   v3 = vsum;
 
+  //
+  // "There is my sum, there is my sum, theeeere is my sum ..."
+  //
+
   for (auto e : v3) { std::cout << e << " "; }
   std::cout << std::endl;
+
+  //
+  // "OOooooh-ooooooh ... oooooh-ooooh ..."
+  //
 
   return 0;
 }
