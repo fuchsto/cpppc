7a23f243096a704fdb0c4656ae60352032292ed9
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2017-01-06 03:09:51 +0100

    Added code samples for WAT session

---
+++ session-wats/02-lexing/main.cc
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+
+int yaba() { return 100; }
+int daba() { return 110; }
+int doo()  { return 011; }
+
+
+int main() {
+  using std::cout;
+  using std::cin;
+  using std::endl;
+
+  int doodeedoo[10];
+  for (int i = 0; i < sizeof(doodeedoo); i++) ??<
+    doodeedoo[i] = 2 * i;
+  %>
+
+  http://www.first-class-url-support.com
+
+  cout << "you don't get it, hm??! ... "
+          "ah, no worries, just enjoy the show." << endl;
+  
+  cout << (5[doodedoo]) << endl;
+
+  cout << (yaba(),daba(),doo()) << endl;
+
+  if (cin>>x,x&2,x<4) { cout << "okee" << endl; }
+
+  if (int ret = dodo()) {
+    cout << "dodo! " << ret << endl;
+  } else {
+    cout << "dodo? " << ret << endl;
+  }
+
+  int x1 = 1;
+  int x2 = 2;
+  if (x1 == 1)
+    if (x2 == 3)
+      (cout << "boopiedoop" << endl);
+  else
+    (cout << "doodiedoo" << endl);
+
+  [](){}();
+
+  return EXIT_SUCCESS;
+}
+
