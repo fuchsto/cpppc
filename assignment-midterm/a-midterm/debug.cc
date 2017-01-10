4648a7b3a25f5d0101bc7955589f574ae53e1a81
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2017-01-10 22:58:13 +0100

    Fixed debug.cc code example

--- assignment-midterm/a-midterm/debug.cc
+++ assignment-midterm/a-midterm/debug.cc
@@ -1,30 +1,45 @@
 
 #include <iostream>
-#include <vector>
+#include <list>
 #include <algorithm>
 #include <numeric>
+#include <future>
 
 
-template <typename Vector, typename UnaryPred>
-Vector & drop(Vector & v, UnaryPred condition) {
-  for (auto i = v.begin(); i != v.end(); ++i) {
+template <typename SeqContainer, typename UnaryPred>
+SeqContainer & drop(SeqContainer & c, UnaryPred condition) {
+  for (auto i = c.begin(); i != c.end(); ++i) {
     if (condition(*i)) {
-      v.erase(i);
+      c.erase(i);
     }
   }
 }
 
 int main() {
 
-  std::vector<int> v();
+  std::list<int> l;
 
+  int x = 100;
   std::generate_n(
-    std::back_inserter(v),
-    []() { std::rand() % 1024; });
+    std::back_inserter(l), 23,
+    [&]() -> int { return ++x; }
+  );
 
-  auto & vd = drop(v, [](int x) -> bool {
-                        return x % 3 == 0;
-                      });
+  for (auto e : l) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+
+  auto f = std::async(
+              std::launch::async,
+              [&]() -> auto { 
+                return drop(l, [](int x) -> bool {
+                  return x % 3 == 0;
+                });
+              }
+           );
+
+  auto ld = f.get();
 
   return EXIT_SUCCESS;
 }
